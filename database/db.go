4101ddfb53cc32d9ec2a1eaf78ba9bba2147345e
Seth Manning <sethmtwo@gmail.com>
2021-07-11 08:52:27 -0700

    7/11/2021 8:52AM

--- database/db.go
+++ database/db.go
@@ -11,7 +11,11 @@ import (
 
 func Init() (*gorm.DB, error) {
 	fmt.Println("Connecting to DB")
-	db, err := gorm.Open(postgres.Open(os.Getenv("DB_URL")), &gorm.Config{})
+	dbURL := fmt.Sprintf(
+		"host=%s user=%s password=%s database=%s",
+		os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"),
+	)
+	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
